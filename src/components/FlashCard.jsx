6231d0bf26fabaa0b9805ecc9262698c3a535640
José Maia <maianeto2014@hotmail.com>
2026-04-03 18:44:58 -0300

    feat: Implemented flip animation

--- src/components/FlashCard.jsx
+++ src/components/FlashCard.jsx
@@ -10,12 +10,16 @@ function FlashCard( {flashCards, currentIndex, showAnswer, toggleAnswer} ) {
 
     return (
         <div onClick={toggleAnswer} className="flash-card card text-center w-50 h-50 mb-4 p-4">
-            <div className="card-body p-2 d-flex align-items-center justify-content-center">
-                <p className="card-text display-6" style={{ fontSize: "clamp(26px, 1.2em, 32px)" }}>
-                    {showAnswer ? currentCard.answer : currentCard.question}
+            <div className={`flash-card-inner card-body p-2 d-flex align-items-center justify-content-center ${showAnswer ? 'is-flipped' : ''}`}>
+                <p className="d-flex align-items-center justify-content-center flash-card-front card-text display-6" style={{ fontSize: "clamp(26px, 1.2em, 32px)" }}>
+                    {currentCard.question}
+                </p>
+                <p className="d-flex align-items-center justify-content-center flash-card-back card-text display-6" style={{ fontSize: "clamp(26px, 1.2em, 32px)" }}>
+                    {currentCard.answer}
                 </p>
             </div>
         </div>
+
     );
 }
 
